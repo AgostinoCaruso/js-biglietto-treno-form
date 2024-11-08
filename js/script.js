6bbbe70006501c3041eacbc34cebe5aa1b8b2900
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-08 18:09:14 +0100

    print on innerHTML value

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 "use strict";
 console.clear();
 
+//variable STRING
+const dNone = "my-d-none";
+
+
 //variables of HTML
 const myContainer = document.querySelector(".my-container");
 //LOGIN
 const formLogin = document.getElementById("login");
 
 const clientName = document.getElementById("clientName");
-// const clientSurname = document.getElementById("clientSurname")
 const ageSelected = document.getElementById("ageSelector");
 const kmTrip = document.getElementById("km");
 const resetForm = document.getElementById("reset");
 //TICKET
 const ticketPanel = document.getElementById("ticket");
+const ticketH2Panel = document.getElementById("ticketH2");
 
-
-
-
+const passengerNameTicket = document.getElementById("passengerNameTicket");
+const discountTicket = document.getElementById("discountTicket");
+const carriageTicket = document.getElementById("carriageTicket");
+const priceTicket = document.getElementById("priceTicket");
 
 //Other stuff
 formLogin.style.backgroundColor ="green";
@@ -25,10 +30,46 @@ formLogin.addEventListener("submit",function (event){
     event.preventDefault();
     
     Operation();
-    ticketPanel.classList.remove("my-d-none");
+    AddValueToHtml();
+    //display ticket
+    ticketPanel.classList.remove(dNone);
+    ticketH2Panel.classList.remove(dNone);
 });
 //reset form and remove ticket panel
-resetForm,addEventListener("reset", () => ticketPanel.classList.add("my-d-none"));
+resetForm,addEventListener("reset", () => {
+    ticketPanel.classList.add(dNone);
+    ticketH2Panel.classList.add(dNone);
+
+    RemoveInnerHtml();
+})
+
+
+function AddValueToHtml(){
+    passengerNameTicket.innerHTML = clientName.value;
+    if(passengerNameTicket.innerHTML == "")
+        passengerNameTicket.innerHTML ="None";
+
+    if(ageSelected.value === "junior"){
+        discountTicket.innerHTML = "20%";
+    }else if(ageSelected.value === "old"){
+        discountTicket.innerHTML = "40%";
+    }else{
+        discountTicket.innerHTML = "Full price";
+    }
+    
+    priceTicket.innerHTML = parseInt(finalPrice).toFixed(2)+ "€";
+    carriageTicket.innerHTML = Math.floor(Math.random()*100+1);
+}
+function RemoveInnerHtml(){
+    passengerNameTicket.innerHTML = "None";
+    priceTicket.innerHTML = "...";
+}
+
+
+
+
+
+
 
 
 //----------------------------------------------------------------------------------------
@@ -45,7 +86,7 @@ console.clear();
 
 function Operation(){
 //check how much does it cost
-finalPrice = (priceTicketKm * parseInt(kmTrip.value)).toFixed(2); //final price before discount
+//finalPrice = (priceTicketKm * parseInt(kmTrip.value)).toFixed(2); //final price before discount
 
 //if statement
 if (ageSelected.value == "junior") {
@@ -54,6 +95,7 @@ if (ageSelected.value == "junior") {
 else if (ageSelected.value =="old") {
     CalculateDiscountPrice(discountSenior);
 } else {
+    finalPrice = (priceTicketKm * parseInt(kmTrip.value)).toFixed(2); //final price before discount
     console.log(`The final cost is: ${finalPrice}€, unfortunatelly at your age of ${ageSelected.value} you cannot access to a discount price!\n
 Your data for this travel is: age:${ageSelected.value}, km: ${kmTrip.value}, final price is: ${finalPrice}€!!`);
 }
